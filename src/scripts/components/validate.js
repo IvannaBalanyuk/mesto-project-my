4fe808bf2d3ef7156214cad4eb1b2594ab9056ff
Ivanna Balanyuk <ivannabalanyuk@yandex.ru>
2023-05-10 17:23:26 +0300

    fix: отредактировать файл README, исправить работу функции enableValidation

--- src/scripts/components/validate.js
+++ src/scripts/components/validate.js
@@ -1,30 +1,25 @@
-import {
-  formSelectors,
- } from '../components/constants.js';
-
-
 // Функция для показа сообщения об ошибке ввода
-  const showInputError = (formElement, inputElement, errorMessage) => {
+  const showInputError = (formElement, inputElement, errorMessage, formObject) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add(formSelectors.inputErrorClass);
+    inputElement.classList.add(formObject.inputErrorClass);
     errorElement.textContent = errorMessage;
-    errorElement.classList.add(formSelectors.errorActiveClass);
+    errorElement.classList.add(formObject.errorActiveClass);
   }
 
 // Функция для скрытия сообщения об ошибке ввода
-  const hideInputError = (formElement, inputElement) => {
+  const hideInputError = (formElement, inputElement, formObject) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove(formSelectors.inputErrorClass);
-    errorElement.classList.remove(formSelectors.errorActiveClass);
+    inputElement.classList.remove(formObject.inputErrorClass);
+    errorElement.classList.remove(formObject.errorActiveClass);
     errorElement.textContent = '';
   }
 
 // Функция для проверки валидности поля ввода
-  const checkInputValidity = (formElement, inputElement) => {
+  const checkInputValidity = (formElement, inputElement, formObject) => {
     if (!inputElement.validity.valid) {
-      showInputError(formElement, inputElement, inputElement.validationMessage);
+      showInputError(formElement, inputElement, inputElement.validationMessage, formObject);
     } else {
-      hideInputError(formElement, inputElement);
+      hideInputError(formElement, inputElement, formObject);
     }
   }
 
@@ -36,36 +31,36 @@ import {
   }
 
 // Функция для переключения состояния кнопки отправки формы (при наличии в форме хотя бы одного невалидного поля - кнопка не активна)
-  const toggleButtonState = (inputList, buttonElement) => {
+  const toggleButtonState = (inputList, buttonElement, formObject) => {
     if (hasInvalidInput(inputList)) {
-      buttonElement.classList.add(formSelectors.inactiveButtonClass);
+      buttonElement.classList.add(formObject.inactiveButtonClass);
     } else {
-      buttonElement.classList.remove(formSelectors.inactiveButtonClass);
+      buttonElement.classList.remove(formObject.inactiveButtonClass);
     };
   }
 
 // Функция для добавления слушателя событий для полей ввода
-  const setEventListeners = (formElement) => {
-    const inputList = Array.from(formElement.querySelectorAll(formSelectors.inputSelector));
-    const buttonElement = formElement.querySelector(formSelectors.submitButtonSelector);
+  const setEventListeners = (formElement, formObject) => {
+    const inputList = Array.from(formElement.querySelectorAll(formObject.inputSelector));
+    const buttonElement = formElement.querySelector(formObject.submitButtonSelector);
 
     inputList.forEach((inputElement) => {
       inputElement.addEventListener('input', function () {
-        checkInputValidity(formElement, inputElement);
-        toggleButtonState(inputList, buttonElement);
+        checkInputValidity(formElement, inputElement, formObject);
+        toggleButtonState(inputList, buttonElement, formObject);
       });
     });
   }
 
 // Функция для включения валидации всех форм страницы
-  const enableValidation = (object) => {
-    const formList = Array.from(document.querySelectorAll(object.formSelector));
+  const enableValidation = (formObject) => {
+    const formList = Array.from(document.querySelectorAll(formObject.formSelector));
 
     formList.forEach(formElement => {
       formElement.addEventListener('submit', evt => {
         evt.preventDefault();
       });
-      setEventListeners(formElement);
+      setEventListeners(formElement, formObject);
     });
   }
 
